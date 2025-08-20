cc4734c2cbe07684a1b0f69fcc910fbe59b6e33c
Shruti Tiwari <163073836+shruti-tiwari761@users.noreply.github.com>
2025-08-20 12:50:18 +0530

    Add files via upload

---
+++ query_data.py
@@ -0,0 +1,53 @@
+import argparse
+# from dataclasses import dataclass
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+
+    # Prepare the DB.
+    embedding_function = GoogleGenerativeAIEmbeddings(google_api_key="AIzaSyAsskJ6QKtwVlTVw3Von1IWMkGuzzCVw4k", model="embedding-001")
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    print(f"Found {len(results)} results with scores: {[score for _, score in results]}")
+    if len(results) == 0:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatGoogleGenerativeAI(google_api_key="AIzaSyAsskJ6QKtwVlTVw3Von1IWMkGuzzCVw4k", model="gemini-1.5-flash")
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+
+if __name__ == "__main__":
+    main()
